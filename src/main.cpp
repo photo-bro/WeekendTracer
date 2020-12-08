707f501bece045bf62b969c88c4a7cb724a017fb
Josh Harmon <photigragraphy@gmail.com>
2020-12-07 21:36:17 -0800

    WIP: Implemented naive "world" and hittable world entities

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,60 @@
-#include "color.h"
-#include "vec3.h"
+#include <chrono>
 #include <iostream>
 
-int main()
-{
-    // Image
+#include "color.h"
+#include "common.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-    const int image_width = 256;
-    const int image_height = 256;
+color ray_color(const ray &r, const hittable &world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+int main() {
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 600;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
     // Render
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n" << std::endl;
 
-    std::cout << "P3\n"
-              << image_width << ' ' << image_height << "\n255\n"
-              << std::endl;
+    auto start_t = std::chrono::high_resolution_clock::now();
 
-    for (auto j = image_height - 1; j >= 0; --j)
-    {
+    for (auto j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (auto i = image_width - 1; i >= 0; --i)
-        {
-            color pixel_color(double(i) / (image_width - 1),
-                              double(j) / (image_height - 1),
-                              0.25);
+        for (auto i = image_width - 1; i >= 0; --i) {
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+            auto pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
-    std::cerr << "\nDone.\n";
+
+    auto end_t = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+
+    std::cerr << "\nRendered in: " << duration / 1000.0 << " seconds\n";
 }
