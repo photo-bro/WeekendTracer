e3126554f0e7a3dbd34b384fbcd4cba9577ef869
Josh Harmon <photigragraphy@gmail.com>
2020-12-08 12:36:30 -0800

    Implement diffuse shading

--- src/common.h
+++ src/common.h
@@ -4,16 +4,20 @@
 #include <limits>
 #include <memory>
 #include <random>
+
 // Usings
+
 using std::make_shared;
 using std::shared_ptr;
 using std::sqrt;
 
 // Constants
+
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility functions
+
 inline double degrees_to_radians(double degrees) { return degrees * pi / 180.0; }
 
 inline double random_double() {
@@ -35,5 +39,6 @@ inline double clamp(double x, double min, double max) {
 }
 
 // Common Headers
+
 #include "ray.h"
 #include "vec3.h"
--- src/vec3.h
+++ src/vec3.h
@@ -34,21 +34,27 @@ class vec3 {
 
     vec3 &operator/=(const double t) { return *this *= 1 / t; }
 
-    double length_squared() const {
-        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
-    }
+    double length_squared() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
 
     double length() const { return sqrt(length_squared()); }
+
+    inline static vec3 random() { return vec3(random_double(), random_double(), random_double()); }
+
+    inline static vec3 random(double min, double max) {
+        return vec3(random_double(min, max), random_double(), random_double(min, max));
+    }
 
    public:
     double e[3];
 };
 
 // Type aliases
+
 using point3 = vec3;  // 3D Point
 using color = vec3;   // RGB Color
 
 // vec3 Utility Functions
+
 inline std::ostream &operator<<(std::ostream &out, const vec3 &v) {
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2] << ' ';
 }
@@ -65,9 +71,7 @@ inline vec3 operator*(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
-inline vec3 operator*(double t, const vec3 &v) {
-    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
-}
+inline vec3 operator*(double t, const vec3 &v) { return vec3(t * v.e[0], t * v.e[1], t * v.e[2]); }
 
 inline vec3 operator*(const vec3 &v, double t) { return t * v; }
 
@@ -78,9 +82,26 @@ inline double dot(const vec3 &u, const vec3 &v) {
 }
 
 inline vec3 cross(const vec3 &u, const vec3 &v) {
-    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
-                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1], u.e[2] * v.e[0] - u.e[0] * v.e[2],
                 u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
 inline vec3 unit_vector(vec3 v) { return v / v.length(); }
+
+inline vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
+vec3 random_unit_vector() { return unit_vector(random_in_unit_sphere()); }
+
+vec3 random_in_hemisphere(const vec3 &normal) {
+    vec3 in_unit_sphere = random_in_unit_sphere();
+    if (dot(in_unit_sphere, normal) > 0.0)
+        return in_unit_sphere;
+    else
+        return -in_unit_sphere;
+}
